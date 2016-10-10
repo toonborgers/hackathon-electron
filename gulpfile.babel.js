e94db64766577b7775af264b0251c878adad0670
Toon Borgers <borgers.toon@gmail.com>
2016-10-10 09:16:59 +0200

    Basic app

---
+++ gulpfile.babel.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let childProcess = require('child_process');
+let electron = require('electron');
+let gulp = require('gulp');
+let jetpack = require('fs-jetpack');
+
+let projectDir = jetpack;
+let destDir = projectDir.cwd('./build');
+
+gulp.task('clean', ()=> {
+    return destDir.dirAsync('.', {empty: true});
+});
+
+gulp.task('run', () => {
+    childProcess.spawn(electron, ['./app'], {stdio: 'inherit'});
+});
