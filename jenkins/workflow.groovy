0e36f20fa316247f083cfb7a3c1168e0575d437f
Toon Borgers <borgers.toon@gmail.com>
2016-10-13 20:10:56 +0200

    test for docker step

--- jenkins/workflow.groovy
+++ jenkins/workflow.groovy
@@ -1,4 +1,32 @@
+REPO = "https://github.com/toonborgers/hackathon-electron"
+IMAGE_FOR_TESTS = "test:1";
+
 stage name: "Build"
 node {
-    git url: "https://github.com/toonborgers/hackathon-electron"
+    checkout();
+    ensureDockerImage();
+}
+
+/**
+ * Helper functions
+ */
+def checkout() {
+    cleanWorkspace()
+    git url: REPO
+}
+
+def cleanWorkspace() {
+    sh 'rm -rf *'
+}
+
+def ensureDockerImage(){
+    def imageExists = executeCommand("docker images -q ${IMAGE_FOR_TESTS}")
+    echo "Image exists: [${imageExists}]"
+}
+
+def executeCommand(String command) {
+    sh "${command} > commandOutput.txt"
+    def commandOutput = readFile("commandOutput.txt").trim()
+    sh "rm commandOutput.txt"
+    commandOutput
 }
