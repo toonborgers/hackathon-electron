fa22147f7a0a7882a04afb6b2aff16e354d89e8b
Toon Borgers <borgers.toon@gmail.com>
2016-10-14 10:08:29 +0200

    Added function on settings service

--- app/scripts/settings/settings.service.js
+++ app/scripts/settings/settings.service.js
@@ -1,3 +1,16 @@
 (function () {
     var electronSettings = require('electron-settings');
+
+    angular.module("app.settings")
+        .service("SettginsService", SettingsService);
+
+    function SettingsService() {
+        return {
+            hasServerData: hasServerData
+        };
+
+        function hasServerData() {
+            return false;
+        }
+    }
 })();
