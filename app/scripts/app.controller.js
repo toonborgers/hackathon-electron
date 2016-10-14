ab85e6f6060eb1c4d75eae62907934cea292d5ba
Toon Borgers <borgers.toon@gmail.com>
2016-10-14 10:50:59 +0200

    Get server data when entered

--- app/scripts/app.controller.js
+++ app/scripts/app.controller.js
@@ -1,12 +1,20 @@
 angular.module('app')
-    .controller("MainController", function (JobsService) {
+    .controller("MainController", function (JobsService, $rootScope, SettingsService) {
         "ngInject";
         var that = this;
 
-        this.getJobs = function () {
-            JobsService.getJobs(that.url, that.user, that.password)
+        var getJobs = function () {
+            JobsService.getJobs()
                 .then(function (result) {
                     that.jobs = result.data.jobs;
                 });
         };
+
+        $rootScope.$on("serverDataEntered", function () {
+            getJobs();
+        });
+
+        if (SettingsService.hasServerData()) {
+            getJobs();
+        }
     });
