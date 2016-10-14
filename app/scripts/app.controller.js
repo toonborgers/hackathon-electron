5b874918e50b2815ee97a1cc704f94e59c0fd138
William <William.Mauclet@staff.telenet.be>
2016-10-14 14:55:07 +0200

    Grid view.

--- app/scripts/app.controller.js
+++ app/scripts/app.controller.js
@@ -4,6 +4,8 @@ angular.module('app')
         var that = this;
         that.serverData = null;
 
+        that.tileDimensions = [];
+
         var getJobs = function () {
             that.serverData = SettingsService.getServerData();
             JobsService.getJobs()
@@ -22,5 +24,52 @@ angular.module('app')
 
         if (SettingsService.hasServerData()) {
             getJobs();
+        }
+
+        this.larger = function(job) {
+            job.height = Math.min(job.height + 1, 2);
+            job.width = Math.min(job.width + 1, 2);
+        };
+
+        this.smaller = function(job) {
+            job.height = Math.max(job.height - 1, 1);
+            job.width = Math.max(job.width - 1, 1);
+        };
+
+        this.getColor = function(job) {
+            var baseColor = that.baseColor(job);
+            if(baseColor == 'blue'){
+                return 'cornflowerblue';
+            }
+            else if (baseColor == 'red'){
+                return 'tomato';
+            }
+            else{
+                return 'darkgrey';
+            }
+        };
+
+        this.baseColor = function(job) {
+            if(job.color != undefined) {
+                return job.color;
+            }
+            else {
+                results = []
+                job.jobs.forEach(function (subjob) {
+                    results.push(that.getColor(subjob))
+                });
+                if(results.indexOf('red') != -1) {
+                    return 'red';
+                }
+                else {
+                    return 'blue';
+                }
+            }
+        };
+        
+        this.promptBuild = function(job) {
+            JobsService.startBuild(job).then(function() {
+                JobsService.getJobs();
+            });
         }
     });
--- app/scripts/jobs/jobs.service.js
+++ app/scripts/jobs/jobs.service.js
@@ -2,7 +2,8 @@ angular.module("app.jobs")
     .service("JobsService", function ($http, $base64, SettingsService) {
         "ngInject";
         return {
-            getJobs: getJobs
+            getJobs: getJobs,
+            startBuild: startBuild
         };
 
         function getJobs() {
@@ -18,10 +19,24 @@ angular.module("app.jobs")
             }
 
             if (user && password) {
-                var headers = {"Authorization": "Basic " + $base64.encode(user + ":" + password)}
+                var headers = {"Authorization": "Basic " + $base64.encode(user + ":" + password)};
                 return $http.get(url, {headers: headers});
             }
 
             return $http.get(url);
+        }
+
+        function startBuild(job) {
+            var serverData = SettingsService.getServerData();
+            var url = serverData.url;
+            var user = serverData.user;
+            var password = serverData.password;
+
+            console.log(user);
+            console.log(password);
+            var headers = {"Authorization": "Basic " + $base64.encode(user + ":" + password)}
+            url +=  "/job/" + job.name.split(" ").join("%20") + "/build";
+
+            return $http.post(url, {headers: headers});
         }
     });
